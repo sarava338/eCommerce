e8ff2204fd81ed1157f3b454a63868670a901cdb
sarava338 <sarava338@gmail.com>
2023-05-27 00:28:06 +0530

    Completed signup api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,11 +3,13 @@ import { createUser } from "../db.js";
 
 const router = express.Router();
 
+//SIGNUP
 router.post("/auth/signup", async (req, res) => {
   createUser({
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
+    isAdmin: req.body.isAdmin
   })
     .then((user) => res.status(201).json(user))
     .catch((err) => res.status(500).json(err));
