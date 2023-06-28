7c0354cbf524f540261fa491050216a4ac1f3864
sarava338 <sarava338@gmail.com>
2023-06-28 23:18:56 +0530

    req -> code

--- src/middleware/log.middleware.js
+++ src/middleware/log.middleware.js
@@ -12,28 +12,3 @@ export const logEveryRequest = (req, res, next) => {
   );
   next();
 };
-
-export const logEveryResponse = (req, res, next) => {
-  const statusCode = res.statusCode;
-  if (statusCode >= 200 && statusCode < 300)
-    Log.success(
-      "---RESPONSE---",
-      "[METHOD]",
-      req.method,
-      "[URL]",
-      req.originalUrl,
-      "[REQ]",
-      statusCode
-    );
-  else if (statusCode >= 400 && statusCode < 600)
-    Log.error(
-      "---RESPONSE---",
-      "[METHOD]",
-      req.method,
-      "[URL]",
-      reqqq.originalUrl,
-      "[CODE]",
-      statusCode
-    );
-  next();
-};
