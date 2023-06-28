35369a2472b4ea60949e1fa3fa6a56d3fa8c015d
sarava338 <sarava338@gmail.com>
2023-06-28 20:27:52 +0530

    Log color was changed for all args

--- src/libraries/Log.js
+++ src/libraries/Log.js
@@ -4,32 +4,28 @@ export default class Log {
   static info = (...args) => {
     console.log(
       `[${new Date().toLocaleString()}]`,
-      chalk.blue(`[INFO]`),
-      ...args
+      chalk.blue(`[INFO]`, ...args)
     );
   };
 
   static success = (...args) => {
     console.log(
       `[${new Date().toLocaleString()}]`,
-      chalk.green(`[SUCCESS]`),
-      ...args
+      chalk.green(`[SUCCESS]`, ...args)
     );
   };
 
   static warn = (...args) => {
     console.log(
       `[${new Date().toLocaleString()}]`,
-      chalk.yellow(`[WARN]`),
-      ...args
+      chalk.yellow(`[WARN]`, ...args)
     );
   };
 
   static error = (...args) => {
     console.log(
       `[${new Date().toLocaleString()}]`,
-      chalk.red(`[ERROR] `),
-      ...args
+      chalk.red(`[ERROR]`, ...args)
     );
   };
 }
