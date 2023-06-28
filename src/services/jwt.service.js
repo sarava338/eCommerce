df47da101a4e68afb3f46d6b6dcedafd5fce7e8a
sarava338 <sarava338@gmail.com>
2023-06-28 21:00:04 +0530

    jwt service and middlewares are seperated

--- src/services/jwt.service.js
+++ src/services/jwt.service.js
@@ -11,35 +11,3 @@ export const getToken = (user) => {
   );
   return { token };
 };
-
-export const verifyToken = (req, res, next) => {
-  const authHeader = req.headers;
-
-  if (!authHeader.token)
-    return res.status(401).json({ message: "Unathorized token" });
-
-  const token = authHeader.token.split(" ")[1];
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    req.user = user;
-    if (err) return res.status(403).json({ message: "Token not valid" });
-  });
-
-  next();
-};
-
-export const verifyTokenAndAdmin = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (!req.user.isAdmin)
-      return res.status(401).json({ message: "Unauthorized admin" });
-    else next();
-  });
-};
-
-export const verifyTokenAndAuthorization = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (!(req.user.id === req.params.id || req.user.isAdmin))
-      return res.status(401).json({ message: "Unauthorized user" });
-    else next();
-  });
-};
