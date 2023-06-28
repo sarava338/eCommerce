c797757b3da6d2977d349fadfb3a5aafac83058b
sarava338 <sarava338@gmail.com>
2023-06-28 20:05:42 +0530

    renamed all routers

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 
 import { config } from "./app.config.js";
 
-import authRouter from "./routes/auth.js";
-import userRouter from "./routes/user.js";
-import productRouter from "./routes/product.js";
-import cartRouter from "./routes/cart.js";
-import orderRouter from "./routes/order.js";
+import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
+import productRouter from "./routes/product.route.js";
+import cartRouter from "./routes/cart.route.js";
+import orderRouter from "./routes/order.route.js";
 import Log from "./libraries/Log.js";
 import {
   logEveryRequest,
