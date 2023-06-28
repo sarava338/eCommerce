49b73264ebabe1ac88c9edebd559854138fc6974
sarava338 <sarava338@gmail.com>
2023-06-28 23:56:53 +0530

    refactor

--- src/contrllers/user.controller.js
+++ src/contrllers/user.controller.js
@@ -2,7 +2,7 @@ import { deleteUserById, updateUserById } from "../models/User.js";
 import { getUserWithoutPassword } from "../helpers/user.help.js";
 
 export const updateUser = (req, res) => {
-  updateUserById(req.user.id, { $set: req.body })
+  updateUserById(req.params.id, { $set: req.body })
     .then((user) => {
       res.status(200).json(getUserWithoutPassword(user));
     })
@@ -10,7 +10,7 @@ export const updateUser = (req, res) => {
 };
 
 export const deleteUser = (req, res) => {
-  deleteUserById(req.user.id)
+  deleteUserById(req.params.id)
     .then((ans) => res.status(204).json(ans))
     .catch((err) => res.status(409).json(err));
 };
--- src/middleware/jwt.middleware.js
+++ src/middleware/jwt.middleware.js
@@ -5,13 +5,13 @@ export const verifyToken = (req, res, next) => {
   const authHeader = req.headers;
 
   if (!authHeader.token)
-    return res.status(401).json({ message: "Unathorized token" });
+    return res.status(400).json({ message: "no token" });
 
   const token = authHeader.token.split(" ")[1];
 
   jwt.verify(token, config.jwtSecret, (err, user) => {
     req.user = user;
-    if (err) return res.status(403).json({ message: "Token not valid" });
+    if (err) return res.status(403).json({ message: "Token not valid", err });
   });
 
   next();
@@ -19,7 +19,7 @@ export const verifyToken = (req, res, next) => {
 
 export const verifyTokenAndAdmin = (req, res, next) => {
   verifyToken(req, res, () => {
-    if (req.user.role === "ADMIN")
+    if (req.user.role !== "ADMIN")
       return res.status(401).json({ message: "Unauthorized admin" });
     else next();
   });
