c518698a7d4a829b5564154c2fd3859d93f93224
sarava338 <sarava338@gmail.com>
2023-06-29 22:53:14 +0530

    getUser modified for 404

--- src/contrllers/admin.controller.js
+++ src/contrllers/admin.controller.js
@@ -2,12 +2,16 @@ import { findAllUsers, findUserById } from "../models/User.js";
 
 export const getUser = (req, res) => {
   findUserById(req.params.id)
-    .then((user) => res.status(200).json(user))
-    .catch((err) => res.status(404).json(err));
+    .then((user) => {
+      if (user === null || user === {})
+        return res.status(404).json({ message: "User not found" });
+      else return res.status(200).json(user);
+    })
+    .catch((err) => res.status(500).json(err));
 };
 
 export const getAllUsers = (req, res) => {
   findAllUsers()
     .then((users) => res.status(200).json(users))
-    .catch((err) => res.status(404).json(err));
+    .catch((err) => res.status(500).json(err));
 };
