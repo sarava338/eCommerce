8c6794759d6b7e6a2469ebe1c2be371ce8a3d521
sarava338 <sarava338@gmail.com>
2023-07-01 11:59:53 +0530

    token cokie was verified

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,13 +2,17 @@ import jwt from "jsonwebtoken";
 import { config } from "../app.config.js";
 
 export const verifyToken = (req, res, next) => {
-  if (req?.headers?.authorization?.startsWith("Bearer")) {
-    const token = req.headers.authorization.split(" ")[1];
+  if (
+    req?.cookies?.token ||
+    req?.headers?.authorization?.startsWith("Bearer")
+  ) {
+    const token =
+      req?.cookies?.token || req?.headers?.authorization?.split(" ")[1];
     jwt.verify(token, config.jwtSecret, (err, user) => {
       req.user = user;
       if (err) return res.status(403).json({ message: "Token not valid", err });
     });
-  } else return res.status(400).json({ message: "No token" });
+  } else return res.status(400).json({ message: "No token found" });
   next();
 };
 
