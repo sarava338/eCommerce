023338b038a04ef1325d6e54c360a34ecb6bef78
sarava338 <sarava338@gmail.com>
2023-07-25 21:23:31 +0530

    Filter was removed for product

--- src/models/Product.js
+++ src/models/Product.js
@@ -6,7 +6,7 @@ const ProductSchema = new mongoose.Schema(
     description: { type: String, required: true },
     img: { type: String },
     price: { type: Number, required: true },
-    category: { type: mongoose.Schema.Types.ObjectId, ref: "Catagory" },
+    category: { type: String },
     size: { type: String },
     color: { type: String },
   },
@@ -29,19 +29,3 @@ export const deleteProductById = async (id) =>
   await ProductModel.findByIdAndDelete(id);
 export const deleteManyProducts = async (condition) =>
   await ProductModel.deleteMany(condition);
-export const getProductsByFilters = async ({
-  minPrice,
-  maxPrice,
-  catagory,
-  brand,
-  color,
-}) =>
-  await ProductModel.find({
-    price: {
-      $gte: minPrice,
-      $lte: maxPrice,
-    },
-    catagory,
-    brand,
-    color,
-  });
