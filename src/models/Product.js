f44f5687beaf8ca47b6cb9d1cf9f3eb42246ab0f
sarava338 <sarava338@gmail.com>
2023-07-25 19:17:26 +0530

    Renamed : Product model's catagories -> catagory

--- src/models/Product.js
+++ src/models/Product.js
@@ -6,7 +6,7 @@ const ProductSchema = new mongoose.Schema(
     description: { type: String, required: true },
     img: { type: String },
     price: { type: Number, required: true },
-    categories: { type: Array },
+    category: { type: Array },
     size: { type: String },
     color: { type: String },
   },
@@ -29,3 +29,19 @@ export const deleteProductById = async (id) =>
   await ProductModel.findByIdAndDelete(id);
 export const deleteManyProducts = async (condition) =>
   await ProductModel.deleteMany(condition);
+export const getProductsByFilters = async ({
+  minPrice,
+  maxPrice,
+  catagory,
+  brand,
+  color,
+}) =>
+  await ProductModel.find({
+    price: {
+      $gte: minPrice,
+      $lte: maxPrice,
+    },
+    catagory,
+    brand,
+    color,
+  });
