bcbe7bd91dd477b2ce3760df06586c4c97cf522c
sarava338 <sarava338@gmail.com>
2023-11-25 11:16:30 +0530

    moved statusCodes to utils/constants.js

--- src/libraries/ErrorHandler.js
+++ src/libraries/ErrorHandler.js
@@ -1,9 +1,3 @@
-export const statusCodes = {
-  OK: 200,
-  UNAUTHORIZED: 401,
-  CONFLICT: 409,
-};
-
 export default class ApiError extends Error {
   constructor(messege, statusCode) {
     super(messege);
