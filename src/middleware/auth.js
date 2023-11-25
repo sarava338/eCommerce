fe90d4ac52028e1504239b99ed9edcf95c3c491f
sarava338 <sarava338@gmail.com>
2023-11-25 15:21:55 +0530

    middlewares refactored with try/catch block

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,32 +1,49 @@
 import jwt from "jsonwebtoken";
 import { config } from "../config.js";
+import { statusCodes } from "../utils/constants.js";
+import ApiError from "../libraries/ErrorHandler.js";
 
 export const verifyToken = (req, res, next) => {
-  if (
-    // req?.cookies?.token ||
-    req?.headers?.authorization?.startsWith("Bearer")
-  ) {
-    const token =
-      // req?.cookies?.token || 
-      req?.headers?.authorization?.split(" ")[1];
-    jwt.verify(token, config.jwtSecret, (err, user) => {
-      req.user = user;
-      if (err) return res.status(403).json({ message: "Token not valid", err });
-    });
-  } else return res.status(400).json({ message: "No token found" });
-  next();
+  try {
+    if (req?.headers?.authorization?.startsWith("Bearer")) {
+      const token = req?.headers?.authorization?.split(" ")[1];
+      jwt.verify(token, config.jwtSecret, (error, user) => {
+        req.user = user;
+        if (error) throw new ApiError(error.message, statusCodes.UNAUTHORIZED);
+      });
+    } else throw new ApiError("No token found", statusCodes.UNAUTHORIZED);
+    next();
+  } catch (error) {
+    res
+      .status(error.statusCode || statusCodes.INTERNAL_SERVER_ERROR)
+      .json({ status: false, error });
+  }
 };
 
 export const verifyTokenAndAdmin = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (req.user.role === "ADMIN") next();
-    else return res.status(401).json({ message: "Unauthorized admin" });
-  });
+  try {
+    verifyToken(req, res, () => {
+      if (req.user.role !== "ADMIN")
+        throw new ApiError("Unauthorized admin", statusCodes.UNAUTHORIZED);
+      next();
+    });
+  } catch (error) {
+    res
+      .status(error.statusCode || statusCodes.INTERNAL_SERVER_ERROR)
+      .json({ status: false, error });
+  }
 };
 
 export const verifyTokenAndUser = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (req.user.role === "USER") next();
-    else return res.status(401).json({ message: "Unauthorized user" });
-  });
+  try {
+    verifyToken(req, res, () => {
+      if (req.user.role !== "USER")
+        throw new ApiError("Unauthorized user", statusCodes.UNAUTHORIZED);
+      next();
+    });
+  } catch (error) {
+    res
+      .status(error.statusCode || statusCodes.INTERNAL_SERVER_ERROR)
+      .json({ status: false, error });
+  }
 };
--- src/middleware/mongoose.js
+++ src/middleware/mongoose.js
@@ -1,8 +1,16 @@
+import ApiError from "../libraries/ErrorHandler.js";
 import { isMongoDbIdValid } from "../utils/validator.js";
+import { statusCodes } from "../utils/constants.js";
 
 export const mongoDbIdValidator = (req, res, next) => {
-  const id = req?.params?.id || req.user.id;
-  if (!isMongoDbIdValid(id))
-    return res.status(404).json({ message: "mongoose id not valid" });
-  else next();
+  try {
+    const id = req?.params?.id || req.user.id;
+    if (!isMongoDbIdValid(id))
+      throw new ApiError("mongoose id not valid", statusCodes.NOT_FOUND);
+    next();
+  } catch (error) {
+    res
+      .status(error.statusCode || statusCodes.INTERNAL_SERVER_ERROR)
+      .json({ status: false, error });
+  }
 };
