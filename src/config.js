c8810aebbedd5328d741f278b3af9c9da6c66589
sarava338 <sarava338@gmail.com>
2023-11-25 11:17:26 +0530

    added mail service env data

--- src/config.js
+++ src/config.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
+  protocol: process.env.PROTOCOL,
+  host: process.env.HOST,
   port: process.env.PORT,
   mongooseUrl: process.env.MONGO_URL,
   apiBasePath: process.env.API_BASEPATH,
